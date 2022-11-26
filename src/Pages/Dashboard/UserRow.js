d4ceef2aba8b28e45757f2a4bb5900a874e53f34
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2022-11-26 09:45:17 +0600

    admin added

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -10,10 +10,16 @@ const UserRow = ({user, refetch}) => {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
             }
         })
-        .then(res => res.json())
+        .then(res => {
+            if(res.status === 403){
+                toast.error('Failed to Make an admin');
+            }
+            return res.json()})
         .then(data => {
-            refetch();
-            toast.success(`Successfully Made an Admin`);
+            if(data.modifiedCount > 0){
+                refetch();
+                toast.success(`Successfully Made an Admin`);
+            }
         })
     }
   return (
