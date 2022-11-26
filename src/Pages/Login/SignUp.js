da7d6ccf563896557c89d42b4c267ec2626c188d
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2022-11-26 08:25:43 +0600

    admin added

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
 
 const SignUp = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -16,6 +17,8 @@ const SignUp = () => {
       ] = useCreateUserWithEmailAndPassword(auth);
 
       const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+      const [token] = useToken(user || gUser);
 
       const navigate = useNavigate();
 
@@ -30,14 +33,13 @@ const SignUp = () => {
     }
 
     if(user || gUser){
-        console.log(user || gUser)
+      navigate('/appointment');
     }
 
     const onSubmit = async (data) => {
         await createUserWithEmailAndPassword(data.email, data.password);
         await updateProfile({ displayName: data.name });
         console.log("update done");
-        navigate('/appointment')
     }
   return (
     <div className='flex h-screen justify-center items-center'>
